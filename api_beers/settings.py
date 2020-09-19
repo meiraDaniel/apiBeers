84bb2378517bb8a52efe8a690d22d73628ab01d7
meiraDaniel <meira_daniel@outlook.com>
2020-09-19 14:51:50 -0300

    adicionando cors

--- api_beers/settings.py
+++ api_beers/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = '=mkns8q3b4!w_rwu+wt=nlgau+c-g4=t&6&(t^q8z)=760q=d-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
